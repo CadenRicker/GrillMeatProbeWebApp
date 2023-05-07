c6ed6d2ef5d187fd5a12579e9f809cc921d951e3
Caden <cadenricker@gmail.com>
2023-05-06 20:17:40 -0600

    display now updates in the background

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask , render_template, make_response, jsonify, Response
 from flask_restful import Api, Resource, reqparse
-import random
+import random ,time, threading , queue
 from display import display
-import time
+
 app = Flask(__name__)
+
 api = Api(app)
 meatProbeDisplays = display()
 parser = reqparse.RequestParser()
 parser.add_argument('number', type=int)
-
+probeTempsQueue = queue.Queue()
 class MeatProbe(Resource):
     def get(self):
         return make_response(render_template('index.html'))
@@ -20,14 +21,25 @@ class dataStream(Resource):
     def get(self):
         def get_numbers():
             while True:
-                number1 = random.randint(1, 100)
-                number2 = random.randint(1, 100)
-                meatProbeDisplays.drawDisplay(number1,number2)
-                yield 'data: %s,%s\n\n' % (number1, number2)
-                print(number1,number2)
-                time.sleep(5)
+                if not probeTempsQueue.empty() :
+                    number1, number2 = probeTempsQueue.get_nowait()
+                    yield 'data: %s,%s\n\n' % (number1, number2)
+                else:
+                   time.sleep(4)                
+                
+                
         return Response(get_numbers(), mimetype='text/event-stream')
-
+def displayProbeValues():
+    while True:
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        meatProbeDisplays.drawDisplay(number1,number2)
+        probeTempsQueue.put((number1, number2))
+        time.sleep(5)
+# Start the background thread when the application starts
+thread = threading.Thread(target=displayProbeValues)
+thread.daemon = True
+thread.start()
 api.add_resource(MeatProbe, '/')
 api.add_resource(dataStream, '/send_numbers')
 
